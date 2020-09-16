7d2baf2fde138f8070c1da80eb41020485dcfe62
Mark Wolgin <wolginmark@gmail.com>
2020-09-16 15:43:37 -0400

    Word lists enumerated

---
+++ scripts/password.js
@@ -0,0 +1,17 @@
+var start=function() {
+
+}
+
+var firstLetter = new Set(['a', 'b', 'c', 'e', 'f', 'g', 'h', 'i', 'n', 'o', 'p', 'r', 's', 't', 'w']);
+var secondLetter = new Set(['a', 'b', 'e', 'f', 'g', 'h', 'i', 'l', 'm', 'o', 'p', 'r', 't', 'v']);
+var thirdLetter = new Set(['a', 'e', 'g', 'h', 'i', 'l', 'o', 'r', 't', 'u', 'v']);
+var fourthLetter = new Set(['a', 'c', 'd', 'e', 'g', 'h', 'i', 'l', 'n', 'o', 'r', 's', 't', 'u']);
+var fifthLetter = new Set(['d', 'e', 'g', 'h', 'k', 'l', 'n', 'r', 't', 'w', 'y']);
+var alpha = new Set(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']);
+
+var wordList = new ["about", "after", "again", "below", "could", "every", "first", "found", "great", 
+                            "house", "large", "learn", "never", "other", "place", "plant", "point", "right", "small", 
+                            "sound", "spell", "still", "study", "their", "there", "these", "thing", "think", "three", 
+                            "water", "where", "which", "world", "write"];
+
+var selectedWord = wordList[Math.random() * wordList.length];
