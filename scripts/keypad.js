3662a8287d7c6c8dd3a1e2d6b2f3df4bfc2c18ba
Timothy Mazzarelli <tmazzar@udel.edu>
2020-09-18 15:30:18 -0400

    Added certain keypad stuff

---
+++ scripts/keypad.js
@@ -0,0 +1,6 @@
+var firstColumn = ["hoop", "TInsideA", "lambda", "awkwardH", "squidKnife", "fancyH", "backwardsC"];
+var secondColumn = ["backwardsE2D", "hoop", "backwardsC", "looper", "blankStar", "fancyH", "upsideDownQMark"];
+var thirdColumn = ["copyright", "noseEye", "looper", "xi", "broken3", "lambda", "blankStar"];
+var fourthColumn = ["six", "backwardsP", "tB", "squidKnife", "xi", "upsideDownQMark", "smiley"];
+var fifthColumn = ["candles", "smiley", "tB", "C", "backwardsP", "worm", "filledStar"];
+var sixthColumn = ["six", "backwardsE2D", "railroad", "ae", "candles", "hSmiley", "omega"];
