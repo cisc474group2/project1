b5c771f4ea0656e53cdfb938598e428000e8bfef
Timothy Mazzarelli <tmazzar@udel.edu>
2020-09-18 18:49:57 -0400

    Playing with buttons, will move back to logic

--- scripts/keypad.js
+++ scripts/keypad.js
@@ -9,9 +9,16 @@ var listOfColumns = [firstColumn, secondColumn, thirdColumn, fourthColumn, fifth
 var selectedColumn = listOfColumns[Math.floor(Math.random() * listOfColumns.length)];
 
 var firstIndex = Math.floor(Math.random() * 3);
-var secondIndex = Math.floor(Math.random() * (4 - firstIndex) + firstIndex);
-var thirdIndex = Math.floor(Math.random() * (4 - secondIndex) + secondIndex);
-var fourthIndex = Math.floor(Math.random() * (4 - thirdIndex) + thirdIndex);
+var secondIndex = Math.ceil(Math.random() * (4 - firstIndex) + firstIndex);
+var thirdIndex = Math.ceil(Math.random() * (5 - secondIndex) + secondIndex);
+var fourthIndex = Math.ceil(Math.random() * (6 - thirdIndex) + thirdIndex);
+
+var firstSymbol = selectedColumn[firstIndex];
+var secondSymbol = selectedColumn[secondIndex];
+var thirdSymbol = selectedColumn[thirdIndex];
+var fourthSymbol = selectedColumn[fourthIndex];
+
+var symbols = [firstSymbol, secondSymbol, thirdSymbol, fourthSymbol];
 
 
 
