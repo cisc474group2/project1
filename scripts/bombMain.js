41e0f3eaaa52a37fa26bc9e9f28beec94ee467d7
Bonnie Skiena <bonnieskiena@gmail.com>
2020-09-19 16:46:44 -0400

    creates a working bomb module that self populates randomly

--- scripts/bombMain.js
+++ scripts/bombMain.js
@@ -1,7 +1,41 @@
-var bombGame=function()
-{
-    var self=this;
+var allIndicatorLabels = [ 'SND', 'CLR', 'CAR', 'IND', 'FRQ', 'SIG', 'NSA', 'MSA', 'TRN', 'BOB', 'FRK'];
+var allBatteryNames = ['#dBattery1', '#dBattery2', '#dBattery3', '#aABattery1', '#aABattery2', '#aABattery3'];
+var allPortNames = ['#PS2Port', '#RJ45Port', '#SerialPort', '#StereoRCAPort',  '#DVIDPort', '#ParallelPort'];
 
-    
-    $('#module1').text('my module');
+var indicatorLabel = '';
+var batteryNames = [];
+var portNames = [];
+var lightOn = true;
+
+var startGame=function()
+{
+    fillBomb();
+    console.log(batteryNames);
+}
+
+// Populates bomb randomly with ports, indicators and batteries 
+function fillBomb() {
+
+    allBatteryNames.forEach(battery => {
+        if(Math.random() < .5){
+            $(battery).hide();
+        } else {
+            batteryNames.push([battery]);
+        }
+    });
+    allPortNames.forEach(port => {
+        if(Math.random() < .5){
+            $(port).hide();
+        } else {
+            portNames.push(port);
+        }
+    });
+
+    if(Math.random() < .5){
+        lightOn = false;
+        $('#indicatorLight').removeClass("on");
+        $('#indicatorLight').addClass("off");
+    }
+    indicatorLabel = allIndicatorLabels[Math.floor(Math.random()*allIndicatorLabels.length)];
+    $('#indicatorLabel').text(indicatorLabel);
 }
