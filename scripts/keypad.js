d34131790d11ea4bbae99c825036ece8c9ae84ba
Timothy Mazzarelli <tmazzar@udel.edu>
2020-09-21 13:52:37 -0400

    Module is set to be placed on bomb

--- scripts/keypad.js
+++ scripts/keypad.js
@@ -1,3 +1,4 @@
+// all of the possible symbols and their columns
 var firstColumn = ["hoop", "TInsideA", "lambda", "awkwardH", "squidKnife", "fancyH", "backwardsC"];
 var secondColumn = ["backwardsE2D", "hoop", "backwardsC", "looper", "blankStar", "fancyH", "upsideDownQMark"];
 var thirdColumn = ["copyright", "noseEye", "looper", "xi", "broken3", "lambda", "blankStar"];
@@ -5,9 +6,12 @@ var fourthColumn = ["six", "backwardsP", "tB", "squidKnife", "xi", "upsideDownQM
 var fifthColumn = ["candles", "smiley", "tB", "C", "backwardsP", "worm", "filledStar"];
 var sixthColumn = ["six", "backwardsE2D", "railroad", "ae", "candles", "hSmiley", "omega"];
 
+//pick a column at random
 var listOfColumns = [firstColumn, secondColumn, thirdColumn, fourthColumn, fifthColumn, sixthColumn];
 var selectedColumn = listOfColumns[Math.floor(Math.random() * listOfColumns.length)];
 
+
+//pick the symbols from that column at random
 var firstIndex = Math.floor(Math.random() * 3);
 var secondIndex = Math.ceil(Math.random() * (4 - firstIndex) + firstIndex);
 var thirdIndex = Math.ceil(Math.random() * (5 - secondIndex) + secondIndex);
@@ -18,9 +22,8 @@ var secondSymbol = selectedColumn[secondIndex];
 var thirdSymbol = selectedColumn[thirdIndex];
 var fourthSymbol = selectedColumn[fourthIndex];
 
-var symbols = [firstSymbol, secondSymbol, thirdSymbol, fourthSymbol];
 
-
+//assign the selected symbols to the buttons at random
 var buttons = ["TopLeftButton", "TopRightButton", "BottomLeftButton", "BottomRightButton"];
 
 var firstButtonIndex = Math.floor(Math.random() * 3);
@@ -35,10 +38,10 @@ var thirdButtonIndex = Math.floor(Math.random() * 1);
 var thirdButtonID = buttons[thirdButtonIndex];
 buttons.splice(thirdButtonIndex, 1);
 
-
 fourthButtonID = buttons[0];
 
 
+// place the image for each symbol on the appropriate button
 var firstButton = document.getElementById(firstButtonID);
 var firstImage = '<img src="../images/' + firstSymbol + '.png" />';
 firstButton.innerHTML = firstImage;
@@ -55,18 +58,23 @@ var fourthButton = document.getElementById(fourthButtonID);
 var fourthImage = '<img src="../images/' + fourthSymbol + '.png" />';
 fourthButton.innerHTML = fourthImage;
 
+
+// game logic
+// makes sure that buttons are clicked in order
 var count = 0;
 firstButton.addEventListener("click", function(){
+    // no other button has been clicked yet
     if(count == 0){
-        firstButton.style.backgroundColor = "green";
+        firstButton.style.backgroundColor = "#28e64f";
         this.disabled = true;
         count++;
     }
 });
 
 secondButton.addEventListener("click", function(){
+    // only one button has been clicked (first button)
     if(count == 1){
-        secondButton.style.backgroundColor = "green";
+        secondButton.style.backgroundColor = "#28e64f";
         count++;
     }
     else{
@@ -80,8 +88,9 @@ secondButton.addEventListener("click", function(){
 });
 
 thirdButton.addEventListener("click", function(){
+    // only first and second buttons have been clicked
     if(count == 2){
-        thirdButton.style.backgroundColor = "green";
+        thirdButton.style.backgroundColor = "#28e64f";
         count++;
     }
     else{
@@ -94,9 +103,10 @@ thirdButton.addEventListener("click", function(){
 });
 
 fourthButton.addEventListener("click", function(){
+    // first second and third buttons have all been clicked
     if(count == 3){
-        fourthButton.style.backgroundColor = "green";
-        count++;
+        fourthButton.style.backgroundColor =  "#28e64f";
+        document.getElementById("lightShow").className = "indicatorLight successGreen";
     }
     else{
         fourthButton.style.backgroundColor = "red";
