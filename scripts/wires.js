ba93d545dd910b3f5832e53c70b949778534b0f0
Sarah Joyce <sjoyce@udel.edu>
2020-09-22 12:44:40 -0400

    randomly generates wire colors now

--- scripts/wires.js
+++ scripts/wires.js
@@ -4,7 +4,10 @@ $(document).ready(function(){
     var odd = new Boolean(serialNumber%2===1);
     var numWires = Math.floor((Math.random() * 4) + 3);
     $('#upperSegmentHeader').html(serialNumber);
-    
+
+    let wireClasses = ['redWire', 'whiteWire', 'blueWire', 'yellowWire', 'blackWire'];
+    let wireColors = [];
+
     if(numWires==3){
         $('.wire-module').html("<div class=\"wire\"></div><div class=\"wire\"></div><div class=\"wire\"></div>");
     }else if(numWires==4){
@@ -15,8 +18,15 @@ $(document).ready(function(){
         $('.wire-module').html("<div class=\"wire\"></div><div class=\"wire\"></div><div class=\"wire\"></div><div class=\"wire\"></div><div class=\"wire\"></div><div class=\"wire\"></div>");
     }
 
+    var list = document.getElementsByClassName('wire');
+
+    for (i = 0; i < numWires; i++) {
+        wireColors.push(wireClasses[Math.floor((Math.random() * 5))]);
+        $(list[i]).addClass(wireColors[i]);
+    }
+
     $('.wire').click(function(){
-        $(this).addClass('cut-wire').removeClass('wire');
+        $(this).addClass('cut-wire').removeClass('redWire').removeClass('whiteWire').removeClass('blueWire').removeClass('yellowWire').removeClass('blackWire');
     });
     
 });
