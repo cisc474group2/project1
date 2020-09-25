9e24d0cdb84cb7ad20bbaa7392d8adca202359f9
samvdam <samvd@udel.edu>
2020-09-25 05:39:40 -0400

    clock is functional now

--- scripts/clockModule.js
+++ scripts/clockModule.js
@@ -1,32 +1,57 @@
-let clockTicks=[0,5,0,0];
-let strikes=0;
+clockTicks=[0,5,0,0];
+strikes=0;
+document.getElementById("innerBorder").innerHTML=clockTicks[0].toString(10)+clockTicks[1].toString(10)+":"+clockTicks[2].toString(10)+clockTicks[3].toString(10);
 
 let tickDown=function(){
-    wait(1000);
-    clockTicks[4]--;
+    clockTicks[3]--;
     updateClock(clockTicks,3);
+    document.getElementById("innerBorder").innerHTML=clockTicks[0].toString(10)+clockTicks[1].toString(10)+":"+clockTicks[2].toString(10)+clockTicks[3].toString(10);
+    if(clock[0]==0 && clock[1]==0 && clock[2]==0 && clock[3]==0){
+        //lose goes here
+    }
 };
 
 let updateClock=function(clock,pos){
     if(clock[pos]<0){
         if(pos==2){
             clock[pos]=5;
+            clock[pos-1]--;
+        }
+        else if(pos==0){
+            let i=0;
+            for(i=0;i<4;i++){
+                clock[i]=0;
+            }
+            pos=1;
         }
         else{
             clock[pos]=9;
+            clock[pos-1]--;
         }
-        clock[pos-1]--;
         updateClock(clock,pos-1);
     }
 };
 
-let start=function(){
-    while(clockTicks[0]!=0 && clockTicks[1]!=0 && clockTicks[2]!=0 && clockTicks[3]!=0){
-        tickDown();
-    }
-    
+let start=function(){ 
+    setInterval(tickDown, 1000);
 }
 
 let addStrike=function(){
     strikes++;
+    if(strikes==1){
+        document.getElementById("upperSegment").innerHTML="X";
+    }
+    else if(strikes==2){
+        document.getElementById("upperSegment").innerHTML="X X";
+    }
+    else{
+        document.getElementById("upperSegment").innerHTML="X X X";
+        //lose goes here
+    }
 }
+
+start();
+
+document.addEventListener("click", function(){
+    addStrike();
+})
