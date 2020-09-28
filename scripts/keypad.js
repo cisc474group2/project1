d92cab9c618a5aa9ef7bd49ecc542c2b2c6e5032
Mark Wolgin <wolginmark@gmail.com>
2020-09-28 13:18:35 -0400

    shoulda pyshed this sooner

--- scripts/keypad.js
+++ scripts/keypad.js
@@ -2,8 +2,8 @@
 var firstColumn = ["hoop", "TInsideA", "lambda", "awkwardH", "squidKnife", "fancyH", "backwardsC"];
 var secondColumn = ["backwardsE2D", "hoop", "backwardsC", "looper", "blankStar", "fancyH", "upsideDownQMark"];
 var thirdColumn = ["copyright", "noseEye", "looper", "xi", "broken3", "lambda", "blankStar"];
-var fourthColumn = ["six", "backwardsP", "tB", "squidKnife", "xi", "upsideDownQMark", "smiley"];
-var fifthColumn = ["candles", "smiley", "tB", "C", "backwardsP", "worm", "filledStar"];
+var fourthColumn = ["six", "backwardsP", "tb", "squidKnife", "xi", "upsideDownQMark", "smiley"];
+var fifthColumn = ["candles", "smiley", "tb", "C", "backwardsP", "worm", "filledStar"];
 var sixthColumn = ["six", "backwardsE2D", "railroad", "ae", "candles", "hSmiley", "omega"];
 
 //pick a column at random
