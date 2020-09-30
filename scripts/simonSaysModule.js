8325d14194268e9a3cc21c13fdb75de83c69f85d
samvdam <samvd@udel.edu>
2020-09-30 05:30:43 -0400

    Simon Says now has colors

--- scripts/simonSaysModule.js
+++ scripts/simonSaysModule.js
@@ -1,14 +1,62 @@
-let length=int(Math.random*10)%3+3;
+let length;
 let sequence=[];
+let seqPos;
+let corPos;
 let setSequence=function(){
+    length=int(Math.random*10)%3+3;
     let i=0;
     for(i;i<length;i++){
         sequence[i]=int(Math.random*10)%4;
     }
-};
+}
+
 let playSequence=function(){
-    let seqPos=0;
+    seqPos=0;
     while(seqPos<length){
-        
+        corPos=0;
+        setInterval(showColor(sequence[seqPos]),5000);
+
+    }
+}
+
+let showColor=function(light){
+    switch(light){
+        case 0:
+
+        case 1:
+
+        case 2:
+
+        case 3:
+            
+    }
+}
+
+let goodClick=function(){
+    corPos++;
+    setAnswer();
+    if(corPos==seqPos){
+        seqPos++;
+    }
+
+}
+
+let badClick=funtion(){
+    addStrike();
+    setSequence();
+    seqPos=0;
+    corPos=0;
+}
+
+let setAnswer=function(correct){
+    switch(correct){
+        case 0:
+
+        case 1:
+
+        case 2:
+
+        case 3:
+
     }
 }
