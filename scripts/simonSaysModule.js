62f8007c10cf8b35cc1624cf4c72e5ecf4a3c0d1
samvdam <samvd@udel.edu>
2020-10-05 13:36:10 -0400

    Simon Says lights up (mostly) correctly

--- scripts/simonSaysModule.js
+++ scripts/simonSaysModule.js
@@ -2,22 +2,30 @@ let length;
 let sequence=[];
 let seqPos;
 let corPos;
+let intervalID;
+
+let startSimon=function(){
+    document.getElementById("blueSimonTop").addEventListener("click", blueClick);
+    document.getElementById("redSimonTop").addEventListener("click", redClick);
+    document.getElementById("yellowSimonTop").addEventListener("click", yellowClick);
+    document.getElementById("greenSimonTop").addEventListener("click", greenClick);
+    setSequence();
+}
+
 let setSequence=function(){
-    length=int(Math.random*10)%3+3;
+    length=Math.floor(Math.random()*10)%3+3;
     let i=0;
-    for(i;i<length;i++){
-        sequence[i]=int(Math.random*10)%4;
+    for(i=0;i<length;i++){
+        sequence[i]=Math.floor(Math.random()*10)%4;
     }
     playSequence();
 }
 
 let playSequence=function(){
-    seqPos=0;
+    seqPos=1;
     setAnswer(sequence[0]);
-    while(seqPos<length){
-        setInterval(showColor(),5000);
-
-    }
+    console.log(seqPos+" "+length);
+    intervalID=setInterval(showColor,5000);
 }
 
 let showColor=function(){
@@ -25,26 +33,43 @@ let showColor=function(){
     for(i=0;i<seqPos;i++){
         switch(sequence[i]){
             case 0:
-                document.getElementById("blueSimon").style.borderColor="rgb(0, 191, 255)";
-                setInterval(document.getElementById("blueSimon").style.borderColor="blue",1000);
+                document.getElementById("blueSimonTop").style.borderBottomColor="rgb(0, 191, 255)";
+                document.getElementById("blueSimonBottom").style.borderTopColor="rgb(0, 191, 255)";
+                setTimeout(function(){
+                    document.getElementById("blueSimonTop").style.borderBottomColor="blue";
+                    document.getElementById("blueSimonBottom").style.borderTopColor="blue";
+                },1000);
                 break;
             case 1:
-                document.getElementById("redSimon").style.borderColor="red";
-                setInterval(document.getElementById("redSimon").style.borderColor= "rgb(206, 7, 7)",1000);
+                document.getElementById("redSimonTop").style.borderBottomColor="red";
+                document.getElementById("redSimonBottom").style.borderTopColor="red";
+                setTimeout(function(){
+                    document.getElementById("redSimonTop").style.borderBottomColor="rgb(206, 7, 7)";
+                    document.getElementById("redSimonBottom").style.borderTopColor="rgb(206, 7, 7)";
+                },1000);
                 break;
             case 2:
-                document.getElementById("yellowSimon").style.borderColor="rgb(250, 250, 98)";
-                setInterval(document.getElementById("yellowSimon").style.borderColor="yellow",1000);
+                document.getElementById("yellowSimonTop").style.borderBottomColor="rgb(250, 250, 98)";
+                document.getElementById("yellowSimonBottom").style.borderTopColor="rgb(250, 250, 98)";
+                setTimeout(function(){
+                    document.getElementById("yellowSimonTop").style.borderBottomColor="rgb(255, 224, 0 )";
+                    document.getElementById("yellowSimonBottom").style.borderTopColor="rgb(255, 224, 0 )";
+                },1000);
                 break;
             case 3:
-                document.getElementById("greenSimon").style.borderColor="rgb(128, 253, 128)";
-                setInterval(document.getElementById("greenSimon").style.borderColor="green",1000);
+                document.getElementById("greenSimonTop").style.borderBottomColor="rgb(128, 253, 128)";
+                document.getElementById("greenSimonBottom").style.borderTopColor="rgb(128, 253, 128)";
+                setTimeout(function(){
+                    document.getElementById("greenSimonTop").style.borderBottomColor="green";
+                    document.getElementById("greenSimonBottom").style.borderTopColor="green";
+                },1000);
                 break;
         }
     }
 }
 
 let goodClick=function(){
+    console.log("good");
     corPos++;
     setAnswer(sequence[corPos]);
     if(corPos==seqPos){
@@ -56,44 +81,99 @@ let goodClick=function(){
 }
 
 let badClick=function(){
+    console.log("bad");
     addStrike();
     setSequence();
-    seqPos=0;
+    clearInterval(intervalID);
+    seqPos=1;
     corPos=0;
 }
 
+let blueClick=function(){
+    document.getElementById("blueSimonTop").style.borderBottomColor="rgb(0, 191, 255)";
+    document.getElementById("blueSimonBottom").style.borderTopColor="rgb(0, 191, 255)";
+    setTimeout(function(){
+        document.getElementById("blueSimonTop").style.borderBottomColor="blue";
+        document.getElementById("blueSimonBottom").style.borderTopColor="blue";
+    },1000);
+}
+
+let redClick=function(){
+    document.getElementById("redSimonTop").style.borderBottomColor="red";
+    document.getElementById("redSimonBottom").style.borderTopColor="red";
+    setTimeout(function(){
+        document.getElementById("redSimonTop").style.borderBottomColor="rgb(206, 7, 7)";
+        document.getElementById("redSimonBottom").style.borderTopColor="rgb(206, 7, 7)";
+    },1000);
+}
+
+let yellowClick=function(){
+    document.getElementById("yellowSimonTop").style.borderBottomColor="rgb(250, 250, 98)";
+    document.getElementById("yellowSimonBottom").style.borderTopColor="rgb(250, 250, 98)";
+    setTimeout(function(){
+        document.getElementById("yellowSimonTop").style.borderBottomColor="rgb(255, 224, 0 )";
+        document.getElementById("yellowSimonBottom").style.borderTopColor="rgb(255, 224, 0 )";
+    },1000);
+}
+
+let greenClick=function(){
+    document.getElementById("greenSimonTop").style.borderBottomColor="rgb(128, 253, 128)";
+    document.getElementById("greenSimonBottom").style.borderTopColor="rgb(128, 253, 128)";
+    setTimeout(function(){
+        document.getElementById("greenSimonTop").style.borderBottomColor="green";
+        document.getElementById("greenSimonBottom").style.borderTopColor="green";
+    },1000);
+}
+
 let setAnswer=function(correct){
+    document.getElementById("blueSimonTop").removeEventListener("click", badClick);
+    document.getElementById("redSimonTop").removeEventListener("click", badClick);
+    document.getElementById("yellowSimonTop").removeEventListener("click", badClick);
+    document.getElementById("greenSimonTop").removeEventListener("click", badClick);
+    document.getElementById("blueSimonTop").removeEventListener("click", goodClick);
+    document.getElementById("redSimonTop").removeEventListener("click", goodClick);
+    document.getElementById("yellowSimonTop").removeEventListener("click", goodClick);
+    document.getElementById("greenSimonTop").removeEventListener("click", goodClick);
     switch(correct){
         case 0:
-            document.getElementById(blueSimon).addEventListener("click", goodClick());
-            document.getElementById(redSimon).addEventListener("click", badClick());
-            document.getElementById(yellowSimon).addEventListener("click", badClick());
-            document.getElementById(greenSimon).addEventListener("click", badClick());
+            console.log("blue");
+            document.getElementById("blueSimonTop").addEventListener("click", goodClick);
+            document.getElementById("redSimonTop").addEventListener("click", badClick);
+            document.getElementById("yellowSimonTop").addEventListener("click", badClick);
+            document.getElementById("greenSimonTop").addEventListener("click", badClick);
+            
             break;
         case 1:
-            document.getElementById(blueSimon).addEventListener("click", badClick());
-            document.getElementById(redSimon).addEventListener("click", goodClick());
-            document.getElementById(yellowSimon).addEventListener("click", badClick());
-            document.getElementById(greenSimon).addEventListener("click", badClick());
+            console.log("red");
+            document.getElementById("blueSimonTop").addEventListener("click", badClick);
+            document.getElementById("redSimonTop").addEventListener("click", goodClick);
+            document.getElementById("yellowSimonTop").addEventListener("click", badClick);
+            document.getElementById("greenSimonTop").addEventListener("click", badClick);
+            
             break;
         case 2:
-            document.getElementById(blueSimon).addEventListener("click", badClick());
-            document.getElementById(redSimon).addEventListener("click", badClick());
-            document.getElementById(yellowSimon).addEventListener("click", goodClick());
-            document.getElementById(greenSimon).addEventListener("click", badClick());
+            console.log("yellow");
+            document.getElementById("blueSimonTop").addEventListener("click", badClick);
+            document.getElementById("redSimonTop").addEventListener("click", badClick);
+            document.getElementById("yellowSimonTop").addEventListener("click", goodClick);
+            document.getElementById("greenSimonTop").addEventListener("click", badClick);
+            
             break;
         case 3:
-            document.getElementById(blueSimon).addEventListener("click", badClick());
-            document.getElementById(redSimon).addEventListener("click", badClick());
-            document.getElementById(yellowSimon).addEventListener("click", badClick());
-            document.getElementById(greenSimon).addEventListener("click", goodClick());
+            console.log("green");
+            document.getElementById("blueSimonTop").addEventListener("click", badClick);
+            document.getElementById("redSimonTop").addEventListener("click", badClick);
+            document.getElementById("yellowSimonTop").addEventListener("click", badClick);
+            document.getElementById("greenSimonTop").addEventListener("click", goodClick);
+            
             break;
     }
 }
 
 
-document.addEventListener("click", function(){
-    document.getElementById("blueSimon").style.border-color="rgb(0, 191, 255)";
+/*document.addEventListener("click", function(){
+    document.getElementById("document.getElementById("blueSimonTop")").style.borderColor);
     badClick();
-})
-setSequence();
+});*/
+
+startSimon();
