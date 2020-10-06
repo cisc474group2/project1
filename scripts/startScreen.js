7833ffab80652cd82c224360b976de1d91675367
Sarah Joyce <sjoyce@udel.edu>
2020-10-06 15:00:31 -0400

    start screen all working

--- scripts/startScreen.js
+++ scripts/startScreen.js
@@ -1,4 +1,6 @@
 var clockTicks = [0, 3, 0, 0];
+var userModules = 4;
+var userStrikes = 3;
 
 document.getElementById("startTimeLeft").addEventListener("click", function(){
     if(clockTicks[0] == 0 && clockTicks[1] == 0){
@@ -41,15 +43,42 @@ document.getElementById("startTimeRight").addEventListener("click", function(){
 });
 
 document.getElementById("startModuleNumLeft").addEventListener("click", function(){
-
+    if(userModules == 2){
+        return;
+    }else{
+        userModules--;
+    }
+    updateStartModules();
 });
 
 document.getElementById("startModuleNumRight").addEventListener("click", function(){
+    if(userModules == 4){
+        return;
+    }else{
+        userModules++;
+    }
+    updateStartModules();
+});
 
+document.getElementById("hardcoreOn").addEventListener("click", function(){
+    document.getElementById('hardcoreOn').classList.add('selected');
+    document.getElementById('hardcoreOff').classList.remove('selected');
+    userStrikes = 1;
+});
+
+document.getElementById("hardcoreOff").addEventListener("click", function(){
+    document.getElementById('hardcoreOff').classList.add('selected');
+    document.getElementById('hardcoreOn').classList.remove('selected');
+    userStrikes = 3;
 });
 
 let updateStartClock = function(){
     document.getElementById("timeText").innerHTML = "Time: " + clockTicks[0].toString(10)+clockTicks[1].toString(10)+":"+clockTicks[2].toString(10)+clockTicks[3].toString(10);
 }
 
+let updateStartModules = function(){
+    document.getElementById("moduleNumber").innerHTML = "Modules: " + userModules.toString(10);
+}
+
 updateStartClock();
+updateStartModules();
